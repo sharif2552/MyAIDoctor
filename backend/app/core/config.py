eb50139a8d295d45d0de1f3ef311b042b9fef9c8
sharif2552 <sharifhossain4888@gmail.com>
2026-04-11 16:09:13 +0600

    feat: added ci/cd

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -22,7 +22,8 @@ class Settings(BaseSettings):
     cors_allow_origins: str = (
         "http://localhost,http://localhost:3000,"
         "http://127.0.0.1,http://127.0.0.1:3000,"
-        "http://20.197.0.41,http://20.197.0.41:3000"
+        "http://20.197.0.41,http://20.197.0.41:3000,"
+        "https://seliseproject.sharifsdevlab.me"
     )
 
 
